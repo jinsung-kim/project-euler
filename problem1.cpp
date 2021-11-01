c12001ca222a1dec983676bdabdb4a77f1ec1656
Jin Kim <jinsungkim1210@gmail.com>
2021-11-01 10:13:25 -0400

    problem 1

---
+++ problem1.cpp
@@ -0,0 +1,19 @@
+/*
+https://projecteuler.net/problem=1
+
+Ans: 233168
+*/
+# include <iostream>
+# define ll long long
+
+using namespace std;
+
+
+
+ll euler1(ll n) {
+    
+}
+
+int main() {
+    cout << euler1(1000) << endl;
+}
