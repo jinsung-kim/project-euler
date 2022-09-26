8b0cfd7360d4adf30bde39d267fd4f86a043a26e
Jin Kim <jinsungkim1210@gmail.com>
2022-09-26 15:38:55 -0700

    .

--- problem2.cpp
+++ problem2.cpp
@@ -3,17 +3,20 @@ https://projecteuler.net/problem=2
 
 Ans: 4613732
 */
-# include <iostream>
-# define ll long long
+#include <iostream>
+#define ll long long
 
 using namespace std;
 
-ll fibSumEvens(ll n) {
+ll fibSumEvens(ll n)
+{
     ll res = 0;
     ll prev = 1, curr = 1;
 
-    while (curr <= 4000000) {
-        if ((curr & 1) == 0) res += curr;
+    while (curr <= 4000000)
+    {
+        if ((curr & 1) == 0)
+            res += curr;
         ll temp = curr;
         curr = prev + curr;
         prev = temp;
@@ -22,6 +25,7 @@ ll fibSumEvens(ll n) {
     return res;
 }
 
-int main() {
+int main()
+{
     cout << fibSumEvens(1000) << endl;
 }
